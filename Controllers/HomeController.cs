72621ccd3ad61f5ae72f87555fc48b611f8964e2
RooRsmoka <chri.disa96@gmail.com>
2022-06-21 12:53:41 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using csharp_blog_frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace csharp_blog_frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(id);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateConBlob(int id)
+        {
+            return View(id);
+        }
+
+        [HttpGet]
+        public IActionResult Update()
+        {
+            return View();
+        }
+    }
+}
